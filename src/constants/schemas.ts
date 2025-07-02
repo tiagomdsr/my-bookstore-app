d9b673e2b7dd29fe8c5007ae78f6aad4c8a7cf66
Tiago Moraes <tmoraes2000@gmail.com>
2025-07-01 21:50:45 -0300

    adjust: fix loginSchema

--- src/constants/schemas.ts
+++ src/constants/schemas.ts
@@ -1,8 +1,6 @@
 import { z } from "zod";
 
-const loginSchema = z.object({
+export const loginSchema = z.object({
 	email: z.string().email("Email inválido"),
 	password: z.string().min(6, "A senha precisa de pelo menos 6 caracteres"),
 });
-
-export type LoginData = z.infer<typeof loginSchema>
